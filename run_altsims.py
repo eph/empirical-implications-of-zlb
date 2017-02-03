c27a5c678373be9387ae3eeaf76faeeec9aa1070
Ed Herbst <ed.herbst@gmail.com>
2017-02-03 12:09:35 -0500

    fix whitspace

--- run_altsims.py
+++
@@ -1,9 +0,0 @@
-import os
-
-cmd = 'mpirun -n 300 ./driver_nomr --infile final-final/by-draw/output{:04d}.json --sim nomr'
-
-for i in range(0, 500):
-
-    print('Running %d' % i)
-    os.system(cmd.format(i))
-
